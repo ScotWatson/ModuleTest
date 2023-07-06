c0726aa682146fb011cf720047d27a1f1271ddc9
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-07-06 17:54:01 -0400

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,63 @@
+/*
+(c) 2022 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+"use strict";
+
+const initPageTime = performance.now();
+
+const asyncWindow = new Promise(function (resolve, reject) {
+  window.addEventListener("load", function (evt) {
+    resolve(evt);
+  });
+});
+
+const asyncErrorLog = (async function () {
+  try {
+    const module = await import("https://scotwatson.github.io/Debug/20230705/ErrorLog.mjs");
+    return module;
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+const asyncModule1 = (async function () {
+  try {
+    const module = await import("https://scotwatson.github.io/ModuleTest/Module1.mjs");
+    return module;
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+const asyncModule2 = (async function () {
+  try {
+    const module = await import("https://scotwatson.github.io/ModuleTest/Module2.mjs");
+    return module;
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+(async function () {
+  try {
+    const modules = await Promise.all( [ asyncWindow, asyncErrorLog, asyncModule1, asyncModule2 ] );
+    start(modules);
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+async function start( [ evtWindow, ErrorLog, Module1, Module2 ] ) {
+  try {
+    const Test = Module1.Test;
+    const ForwardedTest = Module1.funcTest();
+    console.log(Test === ForwardedTest);
+  } catch (e) {
+    ErrorLog.finalCatch({
+      functionName: "start",
+      error: e,
+    });
+  }
+}
